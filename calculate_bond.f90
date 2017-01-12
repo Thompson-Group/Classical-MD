f167fd4fa35344ab1ba6bd055594baa08efc3ebb
Ezekiel Piskulich <piskuliche@ku.edu>
2017-01-12 13:51:04 -0600

    Fixed compiler errors: calculate_bond.f90

--- calculate_bond.f90
+++ calculate_bond.f90
@@ -1,19 +1,21 @@
-subroutine calculate_bond(rx, ry, rz, BT_1, BT_2)
+subroutine calculate_bond(n_atom, n_bond, n_b_type, k, req, rx, ry, rz, fx_b, fy_b, fz_b, bond_table, bond_pe, bond_style)
     use kinds
     implicit none
     ! Variable declarations
     !Loop Integers, Indices, etc.
     integer(kind=ip) :: n_atom, n_bond, n_b_type, i, b_type, b_atom_1, b_atom_2
     !Bonded Parameter Arrays
-    real(kind=dp), allocatable,dimension(n_b_type) :: k, req
+    real(kind=dp), dimension(n_b_type) :: k, req
     !Arrays for storing information, all of n_bond length.
-    real(kind=dp), allocatable,dimension(n_bond):: rx,ry,rz,rinst
+    real(kind=dp), dimension(n_bond):: rx,ry,rz,rinst
     !Arrays for storing information, all of n_atom length.
-    real(kind=dp), allocatable,dimension(n_atom) :: fx_b, fy_b, fz_b
+    real(kind=dp), dimension(n_atom) :: fx_b, fy_b, fz_b
     !Array read in from read_data.f90, stores bonding information.
-    real(kind=dp), allocatable,dimension(n_bond,4)::bond_table
-    real(kind=dp), bond_pe
-    allocate(F(Nbond))
+    real(kind=dp), dimension(n_bond,4)::bond_table
+    real(kind=dp) :: bond_pe, u_bond, r_sep
+    character(len=14) :: bond_style
+    
+   
     
 
     !Bond Energy and Force Calculations
@@ -21,9 +23,9 @@ subroutine calculate_bond(rx, ry, rz, BT_1, BT_2)
         ! Do loop over number of bonds
         DO i = 1, n_bond
             !Index calculations
-            b_type  =bond_table(i+n_bond)
-            b_atom_1=bond_table(i+2*n_bond)
-            b_atom_2=bond_table(i+3*n_bond)
+            b_type  =bond_table(i,1)
+            b_atom_1=bond_table(i,2)
+            b_atom_2=bond_table(i,3)
             !Bond Length Calculations
             !rinst is the instantaneous bond length
             !r_sep is rinst-req
@@ -32,8 +34,8 @@ subroutine calculate_bond(rx, ry, rz, BT_1, BT_2)
                         + (rz(b_atom_1)-rz(b_atom_2))**2 )
             r_sep=rinst(i)-req(b_type)
             !Potential Energy Calculation
-            u_bond(i)=0.5*k(b_type)*r_sep**2
-            bond_pe = bond_pe + u_bond(i)
+            u_bond=0.5*k(b_type)*r_sep**2
+            bond_pe = bond_pe + u_bond
 
             !Force calculation 
             !Calculates the force on atom 1 as:
