7221f8580bafff4fe6eecf139d028ab2dfa05c7c
Ezekiel Piskulich <piskuliche@ku.edu>
2017-01-13 10:53:31 -0600

    Updated to be compliant with common_variables.f90 standards

--- calculate_bond.f90
+++ calculate_bond.f90
@@ -1,20 +1,14 @@
-subroutine calculate_bond(n_atom, n_bond, n_b_type, k, req, x, y, z, rx, ry, rz, fx_b, fy_b, fz_b, bond_table, bond_pe, bond_style)
+subroutine calculate_bond(bond_style)
     use kinds
+    use common_variables
     implicit none
     ! Variable declarations
     !Loop Integers, Indices, etc.
-    integer(kind=ip) :: n_atom, n_bond, n_b_type, i, b_type, b_atom_1, b_atom_2
-    !Bonded Parameter Arrays
-    real(kind=dp), dimension(n_b_type) :: k, req
+    integer(kind=ip) :: i, b_type, b_atom_1, b_atom_2
     !Arrays for storing information, all of n_bond length.
-    real(kind=dp), dimension(n_bond):: rinst
-    !2D Arrays
-    real(kind=dp), dimension(n_atom,n_atom) :: rx, ry, rz
-    !Arrays for storing information, all of n_atom length.
-    real(kind=dp), dimension(n_atom) :: x,y,z,fx_b, fy_b, fz_b
+    real(kind=dp), dimension(n_bonds):: rinst
     !Array read in from read_data.f90, stores bonding information.
-    real(kind=dp), dimension(n_bond,4)::bond_table
-    real(kind=dp) :: bond_pe, u_bond, r_sep
+    real(kind=dp) :: u_bond, r_sep
     character(len=14) :: bond_style
     
    
@@ -23,7 +17,7 @@ subroutine calculate_bond(n_atom, n_bond, n_b_type, k, req, x, y, z, rx, ry, rz,
     !Bond Energy and Force Calculations
     IF (bond_style.eq."harmonic") THEN
         ! Do loop over number of bonds
-        DO i = 1, n_bond
+        DO i = 1, n_bonds
             !Index calculations
             b_type  =bond_table(i,1)
             b_atom_1=bond_table(i,2)
@@ -36,8 +30,8 @@ subroutine calculate_bond(n_atom, n_bond, n_b_type, k, req, x, y, z, rx, ry, rz,
                         + (rz(b_atom_1,b_atom_2)**2) )
             r_sep=rinst(i)-req(b_type)
             !Potential Energy Calculation
-            u_bond=0.5*k(b_type)*r_sep**2
-            bond_pe = bond_pe + u_bond
+            u_bond=0.5*k_r(b_type)*r_sep**2
+            v_b = v_b + u_bond
 
             !Force calculation 
             !Calculates the force on atom 1 as:
@@ -47,11 +41,11 @@ subroutine calculate_bond(n_atom, n_bond, n_b_type, k, req, x, y, z, rx, ry, rz,
             !Calculates the force on atom 2 as:
             !F_2 = -F_1
 
-            fx_b(b_atom_1) = - k(b_type) * r_sep * rx(b_atom_1,b_atom_2) / rinst(i)
+            fx_b(b_atom_1) = - k_r(b_type) * r_sep * rx(b_atom_1,b_atom_2) / rinst(i)
             fx_b(b_atom_2) = - fx_b(b_atom_1)
-            fy_b(b_atom_1) = - k(b_type) * r_sep * ry(b_atom_1,b_atom_2) / rinst(i)
+            fy_b(b_atom_1) = - k_r(b_type) * r_sep * ry(b_atom_1,b_atom_2) / rinst(i)
             fy_b(b_atom_2) = - fy_b(b_atom_1)
-            fz_b(b_atom_1) = - k(b_type) * r_sep * rz(b_atom_1,b_atom_2) / rinst(i)
+            fz_b(b_atom_1) = - k_r(b_type) * r_sep * rz(b_atom_1,b_atom_2) / rinst(i)
             fz_b(b_atom_2) = - fz_b(b_atom_1)
         END DO
     END IF
